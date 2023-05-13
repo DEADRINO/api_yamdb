044a1818c00f16e7b2b7e4574ab6436f88a049e7
der-ton <121458957+der-ton@users.noreply.github.com>
2023-05-13 23:52:14 +0300

    models

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        Category, on_delete=models.DO_NOTHING,
+        related_name='title'
+    )
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.DO_NOTHING)
+    title = models.ForeignKey(Title, on_delete=models.DO_NOTHING)
