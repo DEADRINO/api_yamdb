8f798fd084313297e8fc3f2d25dcd525a8df1076
der-ton <121458957+der-ton@users.noreply.github.com>
2023-05-17 00:07:34 +0300

    Base classes

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from api.models import Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('id', 'name', 'slug')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all())
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'category')
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+# CategoryViewSet, GenreViewSet, TitleViewSet
+from api.models import Category, Genre, Title
+from .serializers import (CategorySerializer, GenreSerializer, TitleSerializer)
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
