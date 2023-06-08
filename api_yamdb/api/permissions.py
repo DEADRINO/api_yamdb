eab333b3ad5b1ab62e287a9c3b325861f0c10631
DEADRINO <121814469+DEADRINO@users.noreply.github.com>
2023-06-08 20:18:52 +0300

    Dabek auth users (#8)
    * auth_users
    
    * readmi
    
    * auth_users_v2

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,20 +2,21 @@ from rest_framework import permissions
 
 
 class IsAdminPermission(permissions.BasePermission):
-    def has_permission(self, request):
+    def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_admin
 
 
 class IsAdminOrReadOnlyPermission(permissions.BasePermission):
-    def has_permission(self, request):
+    def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.is_admin
+            or (request.user.is_authenticated and request.user.is_admin)
+
         )
 
 
 class IsReadOnlyAuthor(permissions.BasePermission):
-    def has_permission(self, request, _):
+    def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,10 +3,10 @@ from django.urls import include, path
 from rest_framework import routers
 
 from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                       ReviewViewSet, TitleViewSet, SignUpView,)
+                       ReviewViewSet, TitleViewSet, SignUpView, UserViewSet)
 
 router = routers.DefaultRouter()
-
+router.register('users', views.UserViewSet, basename='users')
 router.register('categories', CategoryViewSet)
 router.register('genres', GenreViewSet)
 router.register(r'titles', TitleViewSet)
@@ -21,9 +21,7 @@ router.register(
     basename='comments'
 )
 
-
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('auth/signup/', views.SignUpView.as_view()),
-    path('auth/token/', views.GetTokenView.as_view())
-]
+    path('v1/auth/signup/', views.SignUpView.as_view()),
+    path('v1/auth/token/', views.GetTokenView.as_view()),
