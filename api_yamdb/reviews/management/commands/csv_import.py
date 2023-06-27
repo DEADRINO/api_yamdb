3eba0fd37e518dcbbd61f1c181fa941ac8a173a4
der-ton <121458957+der-ton@users.noreply.github.com>
2023-06-27 07:10:37 +0300

    fix (#20)

--- api_yamdb/reviews/management/commands/csv_import.py
+++ api_yamdb/reviews/management/commands/csv_import.py
@@ -1,6 +1,5 @@
 import csv
 from django.core.management.base import BaseCommand
-from core.shortcuts import get_object_or_none
 from reviews.models import (Category, Comment, Genre, GenreTitle,
                             Review, Title, User)
 
@@ -40,50 +39,31 @@ MODELS_FILES = {
 class Command(BaseCommand):
     help = 'For loading data from .csv files'
 
+    def _create_correct_row_fields(self, row):
+        if row.get('author'):
+            row['author'] = User.objects.get(pk=row['author'])
+        if row.get('review_id'):
+            row['review'] = Review.objects.get(pk=row['review_id'])
+        if row.get('title_id'):
+            row['title'] = Title.objects.get(pk=row['title_id'])
+        if row.get('category'):
+            row['category'] = Category.objects.get(pk=row['category'])
+        if row.get('genre'):
+            row['genre'] = Genre.objects.get(pk=row['genre'])
+        return row
+
     def load_data(self):
-        for data in MODELS_FILES.items():
-            with open(data[1]['path']) as file:
-                reader = csv.reader(file)
-                next(reader)  # Advance past the header
+        for key, data in MODELS_FILES.items():
+            path = data['path']
+            model = data['model']
 
-                data[1]['model'].objects.all().delete()
+            model.objects.all().delete()
 
-                for row in reader:
-                    if data[0] in ['category', 'genre']:
-                        item = data[1]['model'](id=row[0],
-                                                name=row[1],
-                                                slug=row[2])
-                        item.save()
-
-                    elif data[0] == 'genre_title':
-                        genre_title = GenreTitle(id=row[0],
-                                                 title_id=row[1],
-                                                 genre_id=row[2])
-                        genre_title.save()
-
-                    elif data[0] == 'comment':
-                        user = get_object_or_none(User, id=row[3])
-                        comment = Comment(id=row[0], review_id=row[1],
-                                          text=row[2], author=user,
-                                          pub_date=row[4])
-                        comment.save()
-
-                    elif data[0] == 'review':
-                        user = get_object_or_none(User, id=row[3])
-                        review = Review(id=row[0], title_id=row[1],
-                                        text=row[2], author=user, score=row[4],
-                                        pub_date=row[5])
-                        review.save()
-
-                    elif data[0] == 'title':
-                        category = get_object_or_none(Category, id=row[3])
-                        title = Title(id=row[0], name=row[1], year=row[2],
-                                      category=category)
-                        title.save()
-                    elif data[0] == 'user':
-                        user = User(id=row[0], username=row[1], email=row[2],
-                                    role=row[3])
-                        user.save()
+            with open(path) as file:
+                csv_read = csv.DictReader(file)
+                for row in csv_read:
+                    row = self._create_correct_row_fields(row)
+                    model.objects.get_or_create(**row)
 
     def handle(self, *args, **kwargs):
         self.load_data()
